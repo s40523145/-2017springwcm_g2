96cdebfe637747d9c7472730909a3fb2c5c2ba8f
s40523145 <s40523145@gm.nfu.edu.tw>
2017-03-17 12:58:29 +0800

    20170317

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# -2017springwcm_g2
+# 2016fallcpa
+40523139的倉儲
+
+https://s40523139.github.io/2016fallcp_hw/#/
+
+https://s40523139.github.io/2016fallcp_hw/blog/
+
---
+++ w4.py
@@ -0,0 +1,8 @@
+'''
+mystring = "40523139"
+mystring = mystring + " test"
+print(mystring)
+'''
+thetext = input("Enter some text\n")
+print ("This is what you entered:" , "test", sep="" )
+print (thetext)
